eb35254ce1c7f02104a9f32ecf7f8e4e1aed04d8
Rishu Kumar <rishukumar.spam@gmail.com>
2025-12-15 11:54:05 +0530

    Add notes and HTML structure for Week 0; implement basic layout for Zerodha app

---
+++ week-1/main.js
@@ -0,0 +1,16 @@
+// let var const
+const a = 2;
+console.log(a);
+// bool+concatenation
+let firstName = "Rishu";
+let age = 22;
+let isMarried = false;
+
+console.log("this person name is " + firstName + " and their age is " + age);
+// conditional statements
+if (isMarried == true) {
+  console.log(firstName + " is married");
+} else {
+  console.log(firstName + " is not married");
+}
+//Loops
