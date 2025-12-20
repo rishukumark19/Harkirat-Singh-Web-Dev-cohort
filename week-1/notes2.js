bc15dba1a8402d93a7d3443a0679cc2f708c9862
Rishu Kumar <rishukumar.spam@gmail.com>
2025-12-20 21:51:49 +0530

    Add file reading examples and asynchronous operations to notes2.js; create demo.txt for testing

--- week-1/notes2.js
+++ week-1/notes2.js
@@ -58,4 +58,51 @@ I/O heavy operations
 I/O (Input/Output) heavy operations 
 refer to tasks in a computer program that involve a lot of data transfer between the program and external systems or devices. 
 These operations usually require waiting for data to be read from or written to sources like disks, networks, databases, or other external devices, which can be time-consuming compared to in-memory computations.
+
+eg:reading a file , HTTP requests , starting a clock
 */
+const fs = require("fs"); //external library (node js file system module) allows u to work with file system on ur computer
+
+// const contents = fs.readFile("demo.txt", "utf-8");      //running asyncronously
+// console.log(contents);
+
+// read from 2 files                                       // running syncronously
+const contents1 = fs.readFileSync("demo.txt", "utf-8");
+console.log(contents1);
+
+const contents2 = fs.readFileSync("demo.txt", "utf-8");
+console.log(contents2);
+
+//Functional arguments     : pasing a function to another function as an argument
+//----------------------
+function sum(a, b) {
+  return a + b;
+}
+function doOperation(a, b, op) {
+  return op(a, b);
+}
+
+console.log(doOperation(1, 2, sum));
+
+//Asynchronous code ,callbacks: allows other operations to run while waiting for an I/O operation to complete .Instead of waiting for the I/O operation to finish, the program can continue executing other tasks and handle the result of the I/O operation once it's ready.
+
+const fs1 = require("fs");
+function print(err, data) {
+  //error first argument
+  if (err) {
+    console.log("Error reading file");
+  } else {
+    console.log(data);
+  }
+}
+fs1.readFile("demo.txt", "utf-8", print); //asynchronous
+fs1.readFile("demo.txt", "utf-8", print); //asynchronous
+
+console.log("This will print before file contents"); //this gets printed first(famous interview question)
+
+//setTimeout : runs a function after a certain time period
+function run() {
+  console.log("I will run after 1s");
+}
+setTimeout(run, 1000);
+console.log("I will run immedietely"); //asynchronous code
